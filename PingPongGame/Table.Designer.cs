365d7cc6657e7a2e97448455a9c7f35ceae75595
Berta Vincze <berta.vincze@gmail.com>
2019-06-18 16:07:07 +0200

    Add basic functionality

---
+++ PingPongGame/Table.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace PingPongGame
+{
+    partial class Table
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.ProgressBar();
+            this.cpu = new System.Windows.Forms.ProgressBar();
+            this.playerScore = new System.Windows.Forms.Label();
+            this.cpuScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Transparent;
+            this.ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ball.Image = global::PingPongGame.Properties.Resources.ball;
+            this.ball.InitialImage = global::PingPongGame.Properties.Resources.ball;
+            this.ball.Location = new System.Drawing.Point(356, 220);
+            this.ball.Margin = new System.Windows.Forms.Padding(0);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(20, 20);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 15;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.White;
+            this.player.Location = new System.Drawing.Point(1, 171);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(20, 115);
+            this.player.TabIndex = 1;
+            // 
+            // cpu
+            // 
+            this.cpu.Location = new System.Drawing.Point(713, 171);
+            this.cpu.Name = "cpu";
+            this.cpu.Size = new System.Drawing.Size(20, 115);
+            this.cpu.TabIndex = 2;
+            // 
+            // playerScore
+            // 
+            this.playerScore.AutoSize = true;
+            this.playerScore.BackColor = System.Drawing.Color.Transparent;
+            this.playerScore.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.playerScore.ForeColor = System.Drawing.Color.White;
+            this.playerScore.Location = new System.Drawing.Point(48, 16);
+            this.playerScore.Name = "playerScore";
+            this.playerScore.Size = new System.Drawing.Size(45, 19);
+            this.playerScore.TabIndex = 3;
+            this.playerScore.Text = "label1";
+            // 
+            // cpuScore
+            // 
+            this.cpuScore.AutoSize = true;
+            this.cpuScore.BackColor = System.Drawing.Color.Transparent;
+            this.cpuScore.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cpuScore.ForeColor = System.Drawing.Color.White;
+            this.cpuScore.Location = new System.Drawing.Point(587, 16);
+            this.cpuScore.Name = "cpuScore";
+            this.cpuScore.Size = new System.Drawing.Size(45, 19);
+            this.cpuScore.TabIndex = 4;
+            this.cpuScore.Text = "label1";
+            // 
+            // Table
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PingPongGame.Properties.Resources.bg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(734, 456);
+            this.Controls.Add(this.cpuScore);
+            this.Controls.Add(this.playerScore);
+            this.Controls.Add(this.cpu);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.ball);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Table";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ping Pong Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Table_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Table_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.ProgressBar player;
+        private System.Windows.Forms.ProgressBar cpu;
+        private System.Windows.Forms.Label playerScore;
+        private System.Windows.Forms.Label cpuScore;
+    }
+}
+
---
+++ PingPongGame/Table.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PingPongGame
+{
+    public partial class Table : Form
+    {
+        public bool IsRunning { get; set; }
+        public bool MoveUp { get; set; }
+        public bool MoveDown { get; set; }
+        public int Speed { get; set; } = 5;
+        public int BallX { get; set; } = 7;
+        public int BallY { get; set; } = 7;
+        public int Score { get; set; } = 0;
+        public int ComputerScore { get; set; } = 0;
+
+        public Table()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.ResizeRedraw, true);
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        private void Table_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                MoveUp = true;
+            }
+            else if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                MoveDown = true;
+            }
+        }
+
+        private void Table_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                MoveUp = false;
+            }
+            else if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                MoveDown = false;
+            }
+            else if (e.KeyCode == Keys.Space && IsRunning == true)
+            {
+                gameTimer.Stop();
+                IsRunning = false;
+            }
+            else if (e.KeyCode == Keys.Space && IsRunning == false)
+            {
+                gameTimer.Start();
+                IsRunning = true;
+            }
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            IsRunning = true;
+            playerScore.Text = "Player Score: " + Score;
+            cpuScore.Text = "CPU Score: " + ComputerScore;
+
+            ball.Top -= BallY;
+            ball.Left -= BallX;
+
+            cpu.Top += Speed;
+
+            if (Score < 5)
+            {
+                if (cpu.Top < 0 || cpu.Top > 340)
+                {
+                    Speed = -Speed;
+                }
+            }
+            else
+            {
+                cpu.Top = ball.Top + 15;
+            }
+
+            if (ball.Left < 0)
+            {
+                ball.Left = 356;
+                BallX = -BallX;
+                BallX += 5;
+                ComputerScore++;
+            }
+
+            if (ball.Left + ball.Width > ClientSize.Width)
+            {
+                ball.Left = 356;
+                BallX = -BallX;
+                BallX -= 5;
+                Score++;
+            }
+
+            if (ball.Top < 0 || ball.Top + ball.Height > ClientSize.Height)
+            {
+                BallY = -BallY;
+            }
+
+            if (ball.Bounds.IntersectsWith(player.Bounds) || ball.Bounds.IntersectsWith(cpu.Bounds))
+            {
+                BallX = -BallX;
+            }
+
+            if (MoveUp == true && player.Top > 0)
+            {
+                player.Top -= 10;
+            }
+
+            if (MoveDown == true && player.Top < 340)
+            {
+                player.Top += 10;
+            }
+
+            if (Score > 10)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("You win!");
+            }
+
+            if (ComputerScore > 10)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("You lose!");
+            }
+        }
+    }
+}
